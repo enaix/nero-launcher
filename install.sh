1339ade31656cc94d6dd5915363bb6df1cd5185b
enaix <enaix@protonmail.com>
2020-11-09 20:04:33 +0300

    Change greeting text in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-read -p "This script will install nero executable\nDefault installation folder is /usr/bin. Would you like to change it? [y/N]" USER_INPUT
+echo "This script will install nero executable."
+
+read -p "Default installation folder is /usr/bin. Would you like to change it? [y/N]" USER_INPUT
 
 if [ "$USER_INPUT" == "y" ] || [ "$USER_INPUT" == "Y" ]; then
 	read -p "Please type the install dir without ending slash:" INSTALL_DIR
@@ -19,3 +21,5 @@ echo "python3 \$NERO_PATH/main.py -d \$NERO_PATH" >> ./nero
 chmod +x ./nero
 
 cp ./nero $INSTALL_DIR/nero
+
+echo "The script was succesfully installed"
